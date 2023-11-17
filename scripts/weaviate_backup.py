9e9b4391fabbb3ac014050461f99957f114f08f8
dachenlian <cclemon.lian@gmail.com>
2023-11-17 04:24:18 +0000

    create tarfile with backup

--- scripts/weaviate_backup.py
+++ scripts/weaviate_backup.py
@@ -2,6 +2,8 @@ import datetime
 from enum import Enum
 import os
 from pathlib import Path
+import shutil
+import tarfile
 
 from dotenv import load_dotenv
 from loguru import logger
@@ -11,6 +13,11 @@ import weaviate
 BASE = Path(__file__).resolve().parent.parent
 
 load_dotenv(str(BASE / ".env"))
+
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:bz2") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 
 class BackupBackend(str, Enum):
@@ -31,6 +38,7 @@ def main(
     """
     current_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
     backup_name = f"{current_date}_{backup_anno}" if backup_anno else current_date
+    BASE_BACKUP_DIR = Path("/mnt/md0/backups/weaviate")
 
     client = weaviate.Client(
         url="http://localhost:8000",
@@ -40,9 +48,20 @@ def main(
         timeout_config=(5, 99999),
     )
     result = client.backup.create(
-        backup_id=backup_name, backend=backend, wait_for_completion=True
+        backup_id=backup_name, backend=backend, wait_for_completion=True, include_classes=['TestContentItem']
     )
     logger.info(result)
+
+    logger.info("Creating tarfile...")
+    make_tarfile(
+        output_filename=str(BASE_BACKUP_DIR / f"{backup_name}.tar.bz2"),
+        source_dir=str(BASE_BACKUP_DIR / backup_name),
+    )
+
+    # logger.info("Deleting unarchived backup...")
+    # shutil.rmtree(Path(BASE_BACKUP_DIR / backup_name))
+
+    logger.info("Done.")
 
 
 if __name__ == "__main__":
